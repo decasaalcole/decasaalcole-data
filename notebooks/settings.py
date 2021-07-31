0fa1b255255441664abff3549ef0f9a93e2fcf91
Pedro Ferrer <pedrof@cartodb.com>
2021-08-01 00:43:54 +0200

    Finished with the Get Data and Pre-process data

--- notebooks/settings.py
+++ notebooks/settings.py
@@ -1,5 +1,8 @@
+# GENERAL
+DATA_FOLDER = '../data'
 # GVA URL AND FILENAME
 DOWNLOAD_URL = 'va/web/centros-docentes/descarga-base-de-datos'
-GVA_DOWNLOAD_AGAIN = False
 GVA_HOST = 'https://ceice.gva.es'
 REGISTER_FILENAME = 'registre.ods'
+# CARTOCiudad FILES
+CARTOCIUDAD_LIST = ['ALICANTE','CASTELLON','VALENCIA']
