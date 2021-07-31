92cc7c23d601f6bdf041df5c72334efefab3b2b2
Pedro Ferrer <pedrof@cartodb.com>
2021-07-31 21:29:16 +0200

    WIP Download data

---
+++ notebooks/settings.py
@@ -0,0 +1,5 @@
+# GVA URL AND FILENAME
+DOWNLOAD_URL = 'va/web/centros-docentes/descarga-base-de-datos'
+GVA_DOWNLOAD_AGAIN = False
+GVA_HOST = 'https://ceice.gva.es'
+REGISTER_FILENAME = 'registre.ods'
