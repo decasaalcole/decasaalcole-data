047f86969e61766ab8b4cfb07d991e1b8da84dda
Pedro Ferrer <pedrof@cartodb.com>
2021-08-05 17:10:13 +0200

    Done with 01 Calculate Postal Code Layer

--- notebooks/settings.py
+++ notebooks/settings.py
@@ -1,8 +1,16 @@
 # GENERAL
 DATA_FOLDER = '../data'
+CARTO_PK_TABLENAME = 'cartociudad_pk'
+CREATE_DATABASE = False
 # GVA URL AND FILENAME
 DOWNLOAD_URL = 'va/web/centros-docentes/descarga-base-de-datos'
 GVA_HOST = 'https://ceice.gva.es'
 REGISTER_FILENAME = 'registre.ods'
 # CARTOCiudad FILES
 CARTOCIUDAD_LIST = ['ALICANTE','CASTELLON','VALENCIA']
+# CNIG Municipal Limits
+CNIG_LIMITS_FILENAME='recintos_municipales_inspire_peninbal_etrs89'
+UPLOAD_LIMITS_AGAIN = False
+# Flags to force GVA data download and CNIG data Unzip
+GVA_DOWNLOAD_AGAIN = False
+CNIG_UNZIP = False
